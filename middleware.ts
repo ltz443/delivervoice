7f4e3ee3a415eec57a816cc8a28745013578f6c3
ltz443 <tonnaleo@gmail.com>
2026-05-11 04:08:58 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,1 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('sb-pyzdzssqvwzsgmpasfgg-auth-token')
-
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-
-  return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/driver/:path*', '/admin/:path*'],
-}
+export {}
